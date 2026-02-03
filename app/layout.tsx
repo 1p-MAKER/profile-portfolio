2765ccd417fe817d940bba16033da8424cc37fa6
1p-MAKER <1pei.nag@gmail.com>
2026-02-03 20:55:23 +0900

    Restore globals.css import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
+import "./globals.css";
 import Link from 'next/link';
 import Footer from '@/components/Footer';
 
